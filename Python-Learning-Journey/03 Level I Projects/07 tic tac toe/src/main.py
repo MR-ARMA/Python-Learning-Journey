e79f0467750cd380e7bd4846eb8ad932b76333b8
Alireza Mahdizadeh <A.L.Mahdizadeh@ut.ac.ir>
2024-01-06 14:55:29 +0330

    Added project level I #7

--- Python-Learning-Journey/03 Level I Projects/07 tic tac toe/src/main.py
+++ Python-Learning-Journey/03 Level I Projects/07 tic tac toe/src/main.py
@@ -1,13 +1,13 @@
 import random
 
+
 class TicTacToe:
     def __init__(self):
         self.board = [' '] *10
         self.player_turn = self.get_random_first_palyer()
 
-    
     def get_random_first_palyer(self, ):
-        return random.choice(['x', 'o'])
+        return random.choice(['X', 'O'])
         
     def show_board(self):
         print('\n')
@@ -22,16 +22,16 @@ class TicTacToe:
         return self.player_turn
     
     def is_board_fill(self):
-        return ' ' not in self.board
+        return ' ' not in self.board[1: ]
     
     def fix_spot(self, cell, player):
         self.board[cell] = player
     
     def has_player_won(self, player):
         win_combination = [
-            [1, 2, 3], [4, 5, 6], [7, 8, 9],
-            [1, 4, 7], [2, 5, 8], [3, 6, 9],
-            [1, 5, 9], [3, 5, 7]
+            [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
+            [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
+            [1, 5, 9], [3, 5, 7] # diagonals
         ]
 
         for combination in win_combination:
@@ -44,7 +44,7 @@ class TicTacToe:
         while True:
             self.show_board()
             try:
-                cell = int(input(f"Player {self.player_turn}, Enter the cell number: "))
+                cell = int(input(f"Player {self.player_turn} you turn, Enter the cell number: "))
 
                 # Check if cell is in the allowed range and is empty
                 if cell in range(1, 10) and self.board[cell] == ' ':
@@ -52,22 +52,22 @@ class TicTacToe:
 
                     if self.has_player_won(self.player_turn):
                         print(f"Player {self.player_turn} wins!")
+                        print(f'player {self.swap_player_turn()} you are always are a loser!!!')
                         break
 
                     if self.is_board_fill():
-                        print("It's a Draw!")
+                        print("Gameover and nobody win but why you do that?!")
                         break
 
                     self.swap_player_turn()
 
                 else:
-                    print("Invalid input, please try again.")
+                    print("Invalid input, please try again.(Enter valit input like a gentleman)")
             except ValueError:
-                print("Invalid input, please enter a number between 1 and 9.")
+                print("\nInvalid input, please enter a number between 1 and 9.")
+                print("we'll treat you with respect, please do the same in return. if your behaviour is unacceptable we may refuse to serve you)")
 
 
-
-    
 if __name__ == "__main__":
     game = TicTacToe()
     game.start()
